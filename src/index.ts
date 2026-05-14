8ce439888af1c2a6c6c9269e1bc0d5f4e18515a7
Ilya Kalutskii <ilyakalutskii@gmail.com>
2026-05-14 21:24:23 +0400

    feat: export hono.logging module in index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ export * from './http/http.resolvers';
 export * from './http/http.schemas';
 
 export * from './hono/hono.execution';
+export * from './hono/hono.logging';
 export * from './hono/hono.respond';
 
 export * from './utilities/datetime.utilities';
