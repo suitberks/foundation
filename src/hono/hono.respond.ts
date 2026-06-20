663b2d7aad344ec10c5cd89d8cbaca2ef51b0e90
Ilya Kalutskii <ilyakalutskii@gmail.com>
2026-06-20 13:41:03 +0400

    feat: update serialization type in respond function to use AsQuery

--- src/hono/hono.respond.ts
+++ src/hono/hono.respond.ts
@@ -2,7 +2,7 @@ import type { Context, TypedResponse } from 'hono';
 
 import { success } from '@/http/http.factory';
 import type { APIError, APISuccess, SuccessStatusCode } from '@/http/http.schemas';
-import type { SerializeDates } from '@/utilities/serialization.utilities';
+import type { AsQuery } from '@/utilities/serialization.utilities';
 
 /**
  * Wraps c.json with a typed success payload / (or void data) & possible APIError response.
@@ -11,6 +11,6 @@ import type { SerializeDates } from '@/utilities/serialization.utilities';
 export function respond<T extends object = Record<string, never>, S extends SuccessStatusCode = SuccessStatusCode>(
   c: Context,
   options: { status: S; data?: T }
-): Response & TypedResponse<APISuccess<SerializeDates<T>> | APIError, S, 'json'> {
+): Response & TypedResponse<APISuccess<AsQuery<T>> | APIError, S, 'json'> {
   return c.json(success({ status: options.status, data: (options.data ?? {}) as T }), options.status) as never;
 }
