4658c64cb5fdc4c0be8921dfc09de9cf9e494dc7
Ilya Kalutskii <ilyakalutskii@gmail.com>
2026-06-20 13:56:48 +0400

    feat: remove atLeastOneDefined refinement and associated error message

--- src/validation/validation.messages.ts
+++
@@ -1,3 +0,0 @@
-// Default error message about at least one key being required.
-export const AT_LEAST_ONE_DEFINED_ERROR = (keys: (string | number | symbol)[]) =>
-  `At least one of the specified keys must be defined: ${keys.join(', ')}`;
--- src/validation/validation.refiners.ts
+++ src/validation/validation.refiners.ts
@@ -1,36 +1,10 @@
 import z from 'zod';
 
-import { AT_LEAST_ONE_DEFINED_ERROR } from './validation.messages';
-import type { AtLeastOne } from './validation.types';
-
-/**
- * Refines a Zod object schema to ensure that at least one of its keys is defined.
- * This is useful for validating objects where at least one property must be present.
- *
- * ```ts
- * const mySchema = z.object({
- *   a: z.string()
- *   b: z.number()
- * }).refine(atLeastOneDefined);
- * ```
- */
-export const atLeastOneDefined = <T extends z.ZodRawShape>(
-  schema: z.ZodObject<T>
-): z.ZodType<AtLeastOne<z.infer<z.ZodObject<T>>>> => {
-  const keys = Object.keys(schema.shape) as (keyof T)[];
-
-  return schema
-    .partial()
-    .refine((value) => keys.some((key) => (value as Record<string, unknown>)[key as string] != null), {
-      message: AT_LEAST_ONE_DEFINED_ERROR(keys),
-    }) as unknown as z.ZodType<AtLeastOne<z.infer<z.ZodObject<T>>>>;
-};
-
 /**
  * Transforms a string to a number (when passing query parameters).
  * Example usage: `asQueryNumber(z.number())('123') = 123`
  */
-export const asQueryNumber = <T extends z.ZodNumber>(schema: T) =>
+export const asQueryNumber = <T extends z.ZodNumber | z.ZodInt>(schema: T) =>
   z.preprocess((v) => (typeof v === 'string' ? Number(v) : v), schema);
 
 /**
