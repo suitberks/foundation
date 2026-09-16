7464fc517245b22a31d5d5bd0d28f5c06d9e67e6
ESB Group <242801576+esb-group-git@users.noreply.github.com>
2026-09-16 22:55:04 +0400

    Remove utility modules and their specifications

--- src/utilities/type.utilities.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Utility type that simplifies a given type T by flattening its structure.
- * This is particularly useful for improving the readability of complex types.
- */
-export type Simplify<T> = { [K in keyof T]: T[K] } & {};
